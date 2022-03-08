3ce39408b87d0eaa96b57cbf24be8abf12956615
Piousangel <yyh7654@gmail.com>
2022-03-08 23:38:37 +0900

    3/8

---
+++ Baekjoon/10950번.py
@@ -0,0 +1,10 @@
+cnt = int(input())
+for x in range(cnt) :
+    a, b = map(int, input().split())
+    print(a+b)
+
+
+# cnt = int(input())
+# for x in range(cnt) :
+#     a, b = input().split()
+#     print(int(a)+int(b))
