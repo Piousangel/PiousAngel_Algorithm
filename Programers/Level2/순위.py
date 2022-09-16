c5510ba109bb5862cf2a6b899d12f09f22376862
양현석 <yyh7654@gmail.com>
2022-09-16 16:03:44 +0900

    Update 순위.py

--- Programers/Level2/순위.py
+++ Programers/Level2/순위.py
@@ -1,25 +1,36 @@
-def solution(info, query):
-    answer = []
+from collections import defaultdict
+from collections import deque
+
+def solution(id_list, report, k) :
+    answer = [0] * len(id_list)
     
-    for qinfo in query :
-        query_list = list(map(str, qinfo.split(" and ")))
-        food, score = query_list[-1].split(" ")
-        del query_list[-1]
-        query_list.append(food)
-        query_list.append(score)
-        # print(query_list)
-        cnt = 0
-        for col in info : 
-            info_list = list(map(str, col.split(" ")))
-          
-            for i in range(len(query_list)-1):
-                if query_list[i] == '-' :
-                    continue                
-                if query_list[i] != info_list[i] :
-                    break
-            else :
-                if int(query_list[-1]) <= int(info_list[-1]) :
-                    cnt += 1
-        answer.append(cnt)
+    mapping= defaultdict()
+    idx = 0
+    
+    graph = [ [] for _ in range(len(id_list))]
+    
+    for name in id_list :
+        mapping[name] = idx
+        idx += 1
         
+    dic = {}
+    temp_list = []
+    
+    for info in report :
+        a, b = map(str, info.split(" "))
+        if mapping[b] not in graph[mapping[a]] :
+            graph[mapping[a]].append(mapping[b])
+            if mapping[b] not in dic :
+                dic[mapping[b]] = 1
+            else :
+                dic[mapping[b]] += 1
+                if dic[mapping[b]] >= k and mapping[b] not in temp_list :
+                    temp_list.append(mapping[b])
+                    
+    for i in range(len(graph)) :
+        if len(graph[i]) == 0 :
+            continue
+        for j in graph[i] :
+            if j in temp_list:
+                answer[i] += 1
     return answer
