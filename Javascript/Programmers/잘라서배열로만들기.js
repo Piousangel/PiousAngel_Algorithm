5230303dcdf25c97bee6214bdd5c4b1f2bf0feb5
양현석 <yyh7654@gmail.com>
2022-10-01 22:53:07 +0900

    10/1

---
+++ Javascript/Programmers/잘라서배열로만들기.js
@@ -0,0 +1,18 @@
+function solution(my_str, n) {
+    let answer = [];
+    let cnt = 0;
+    let temp = "";
+    for(let i = 0 ; i < my_str.length ; i ++) {
+        cnt ++;
+        temp += my_str[i];
+        if (cnt % n === 0) {
+            answer.push(temp)
+            temp = "";
+        } 
+    }
+    if (temp !== ""){
+        answer.push(temp)
+    }
+    
+    return answer;
+}
