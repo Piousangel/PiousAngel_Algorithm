e27e74a33d5b4174597572615421d06f31328711
양현석 <yyh7654@gmail.com>
2022-12-26 23:12:58 +0900

    Add 문자열뒤집기, 나이출력, 배열의평균값

---
+++ Javascript/Programmers/문자열뒤집기.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var answer = '';
+    answer = reverseString(my_string);
+    return answer;
+}
+
+function reverseString(str) {
+  if (str === "")
+    return "";
+  else
+    return reverseString(str.substr(1)) + str.charAt(0);
+}
