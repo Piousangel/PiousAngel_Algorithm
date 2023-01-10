da28afdc018afe4daf8ab337ee1fa31ea5f860f1
양현석 <yyh7654@gmail.com>
2023-01-11 04:47:17 +0900

    Update 가장가까운글자.js

--- Javascript/Programmers/가장가까운글자.js
+++ Javascript/Programmers/가장가까운글자.js
@@ -5,14 +5,13 @@ function solution(s) {
     [...s].forEach((str) => {
         if (!stack.includes(str)) {
             ans.push(-1);
-        }
-
+        };
+        
         if (stack.includes(str)) {
             ans.push(stack.length - stack.lastIndexOf(str));
-        }
+        };
 
         stack.push(str);
     });
-
     return ans;
 }
