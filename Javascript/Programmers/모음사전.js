af88086759d003f238baf0a16923b6bf2b401dab
양현석 <yyh7654@gmail.com>
2023-01-14 16:23:34 +0900

    Add 모은사전

---
+++ Javascript/Programmers/모음사전.js
@@ -0,0 +1,23 @@
+function solution(word) {
+    var answer = 0;
+    
+    alpha = ['A','E','I','O','U'];
+    arr = []
+    
+    function dfs(st, alpha, idx) {
+        
+        if (idx == 6){
+            return;
+        }
+        if (st != ''){
+            arr.push(st);
+        }
+        for(let i = 0; i < alpha.length; i++){
+            dfs(st + alpha[i], alpha, idx + 1)
+        }
+        
+    }
+    dfs('', alpha, 0);    
+    answer = arr.indexOf(word) + 1
+    return answer;
+}
