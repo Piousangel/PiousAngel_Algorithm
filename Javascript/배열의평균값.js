9c5d3b3ff906773524699673b10b3c47ebea14b1
양현석 <yyh7654@gmail.com>
2023-01-16 12:07:05 +0900

    2차원 배열

--- Javascript/배열의평균값.js
+++ Javascript/배열의평균값.js
@@ -4,6 +4,12 @@ function solution(numbers) {
     for(const num of numbers){
         total += parseFloat(num);
     }
+    
+    const row = 5;
+    const col = 5;
+    const  arr = Array.from(new Array(col), () => new Array(row).fill(0));
+    console.log(arr)
+    
     answer = total / numbers.length;
     return answer;
 }
