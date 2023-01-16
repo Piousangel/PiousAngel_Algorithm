e50c0784cef584bd4eba1789ccec60a9df91b01b
양현석 <yyh7654@gmail.com>
2023-01-16 22:12:59 +0900

    Add soomgo coding test Quiz

---
+++ Javascript/codility/soongoQ2.js
@@ -0,0 +1,37 @@
+// you can write to stdout for debugging purposes, e.g.
+// Q2
+function solution(N) {
+    
+    let tempArr = [];
+    let tempSum = [];
+    let Arr = Array.from(String(N));
+    tempArr = [...Arr];
+
+    for(let i = 0; i < Arr.length; i++){
+        if(Arr[i] === '5'){
+            let tempNum1 = tempArr.slice(0,i)
+            let tempNum2 = tempArr.slice(i+1, Arr.length);
+            let temp = [...tempNum1 , ...tempNum2];
+            // console.log(temp)
+            tempSum.push(Number(temp.join('')))
+            
+        }
+    }
+
+    const answer = Math.max(...tempSum);
+    return answer
+
+}
+
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+// Q1
+function solution(S, K) {
+
+    const date = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+    let tempIdx = date.indexOf(S);
+    tempIdx = (tempIdx + K) % 7;
+    return date[tempIdx]
+    // Implement your solution here
+}
