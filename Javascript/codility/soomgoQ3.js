40e83ecc7ca9d29545fd91bd265572a11609f217
양현석 <yyh7654@gmail.com>
2023-01-20 00:00:15 +0900

    Update soomgoQ3.js

--- Javascript/codility/soomgoQ3.js
+++ Javascript/codility/soomgoQ3.js
@@ -1,5 +1,3 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
 // Q3
 function solution(S) {
     const tempArr = S.split("\n")
@@ -28,9 +26,9 @@ function solution(S) {
         let expansion = section[0].split('.')[1];
         let location = section[1];
         let date = section[2];
-        // console.log(expansion)
         let name = '';
         const dateArr = picMap.get(location).indexOf(section[2]);
+         
         if (picMap.get(location).length + 1 >= 10){
             if(dateArr + 1 < 10){
                 name = location + '0' + String(dateArr+1) + '.' + expansion;
@@ -49,6 +47,6 @@ function solution(S) {
             answer += name;
         
     }
-    // console.log(answer)
+    
     return answer;
 }
