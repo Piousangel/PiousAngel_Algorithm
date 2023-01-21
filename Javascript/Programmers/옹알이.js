9e5a42bd13040f22bc2496c8430ea8cfaa46842c
양현석 <yyh7654@gmail.com>
2023-01-21 14:23:14 +0900

    add 옹알리

---
+++ Javascript/Programmers/옹알이.js
@@ -0,0 +1,23 @@
+function solution(babbling) {
+    let answer = 0;
+
+    const arr = ["aya", "ye", "woo", "ma"];
+
+    for (let i = 0; i < babbling.length; i++) {
+        let babble = babbling[i];
+
+        for (let j = 0; j < arr.length; j++) {
+            if (babble.includes(arr[j].repeat(2))) {
+                break;
+            }
+
+            babble = babble.split(arr[j]).join(" ");
+        }
+
+        if (babble.split(" ").join("").length === 0) {
+            answer += 1;
+        }
+    }
+
+    return answer;
+}
