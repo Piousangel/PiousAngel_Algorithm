db6800aa33a99585ece4776f8a22997c4a89b3a1
양현석 <yyh7654@gmail.com>
2023-03-26 13:43:04 +0900

    heap 더맵게

---
+++ Javascript/Programmers/더맵게.js
@@ -0,0 +1,25 @@
+function solution(heights) {
+    let answer = [];
+    let len = heights.length;
+    let idx = 0;
+
+    for (let i = len - 1; i > 0; i--) {
+        let compare = heights[i];
+        let subArr = heights.slice(0, i);
+        let temp = subArr.reverse().find((num) => num > compare);
+
+        if (temp !== undefined) {
+            subArr.reverse().map((num, i) => {
+                if (num === temp) {
+                    idx = i + 1;
+                }
+            });
+        } else {
+            idx = 0;
+        }
+        answer.unshift(idx);
+    }
+
+    answer.unshift(0);
+    return answer;
+}
